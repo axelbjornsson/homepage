47d265b574c0161ba73b63171a734a0b07f04520
Axel Björnsson <axelbjornsson@gmail.com>
2019-07-11 22:02:25 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.ccd14079f30889d97fdfd82c4d4e9019.js"
 );
 
 self.addEventListener('message', (event) => {
